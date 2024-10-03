b3fcab5667c38ea140c957dab9c041c0a6dc54eb
morry6388 <shangrilasars6388@gmail.com>
2024-10-03 15:17:00 +0900

    不要文字を削除

--- src/main/java/com/example/twitterclone/config/AuthenticationConfig.java
+++ src/main/java/com/example/twitterclone/config/AuthenticationConfig.java
@@ -1,4 +1,4 @@
-""package com.example.twitterclone.config;
+package com.example.twitterclone.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
